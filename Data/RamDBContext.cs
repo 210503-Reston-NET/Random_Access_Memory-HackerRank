8f6f1e720a0d1ff7799758a2c63f8cd06ab95721
phobos42 <garrettruffner42@gmail.com>
2021-05-27 17:23:25 -0400

    fixes for so many things

--- Data/RamDBContext.cs
+++ Data/RamDBContext.cs
@@ -1,13 +1,15 @@
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Models;
 namespace Data
 {   
     public class RamDBContext : DbContext {
         public RamDBContext (DbContextOptions<RamDBContext> options) : base (options) { }
-        public DbSet<EntityName> EntityNames { get; set; }
+        public DbSet<TaskItem> TaskItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
-            base.OnModel.Creating(modelBuilder);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
