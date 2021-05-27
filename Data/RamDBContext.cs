57e5720c03cb274eb9d12cb6d72c86bf2d0601a9
phobos42 <garrettruffner42@gmail.com>
2021-05-27 17:03:25 -0400

    🗃

--- Data/RamDBContext.cs
+++ Data/RamDBContext.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 namespace Data
-{
-    public class RamDBContext
-    {
-        
+{   
+    public class RamDBContext : DbContext {
+        public RamDBContext (DbContextOptions<RamDBContext> options) : base (options) { }
+        public DbSet<EntityName> EntityNames { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModel.Creating(modelBuilder);
+        }
     }
 }
