e64d7a900a2f85c8370d0ed016b1fe47f83fa65b
phobos42 <garrettruffner42@gmail.com>
2021-05-27 17:43:47 -0400

    config stuff

--- RAMWebUI/Startup.cs
+++ RAMWebUI/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace RAMWebUI
 {
@@ -23,7 +25,20 @@ namespace RAMWebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<RamDBContext>(options =>options.UseNpgsql(parseElephantSQLURL(this.Configuration.GetConnectionString("RamDB"))));
+            services.AddScoped<Database>();
+            services.AddControllersWithViews();           
+        }
+        public static string parseElephantSQLURL(string uriString)
+        {
+            var uri = new Uri(uriString);
+            var db = uri.AbsolutePath.Trim('/');
+            var user = uri.UserInfo.Split(':')[0];
+            var passwd = uri.UserInfo.Split(':')[1];
+            var port = uri.Port > 0 ? uri.Port : 5432;
+            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
+                uri.Host, db, user, passwd, port);
+            return connStr;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
