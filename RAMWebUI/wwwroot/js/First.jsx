587a21ad9d2e1e93046b32f27b2e7a0a58a6eed3
Mykel Valadez <valadezmykel@gmail.com>
2021-05-27 14:45:52 -0700

    messed around with react might keep it idk

---
+++ RAMWebUI/wwwroot/js/First.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+    <>
+        Hello
+ 
+    </>
+    )
+}
+
+ReactDOM.render(
+    React.createElement(Home, null),
+    // document.getElementById('content'),
+);
