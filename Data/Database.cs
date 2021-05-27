f180b788a631e8a9b682acb4d6f0f4bc200ee6bb
phobos42 <garrettruffner42@gmail.com>
2021-05-27 16:50:02 -0400

    :file:

---
+++ Data/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data
+{
+    public class Database
+    {
+        private RamDBContext _context;
+
+        public Database(RamDBContext context){
+            _context = context;
+        }
+
+
+    }
+}
