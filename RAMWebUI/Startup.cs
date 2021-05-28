0574b9cacbc591d429209160fd350d896a277cc1
phobos42 <garrettruffner42@gmail.com>
2021-05-28 10:36:13 -0400

    add scoped bl

--- RAMWebUI/Startup.cs
+++ RAMWebUI/Startup.cs
@@ -14,6 +14,7 @@ using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
 using React.AspNet;
 using Data;
 using Microsoft.EntityFrameworkCore;
+using RAMBL;
 
 namespace RAMWebUI
 {
@@ -39,6 +40,7 @@ namespace RAMWebUI
             services.AddControllersWithViews();
             services.AddDbContext<RamDBContext>(options =>options.UseNpgsql(parseElephantSQLURL(this.Configuration.GetConnectionString("RamDB"))));
             services.AddScoped<Database>();
+            services.AddScoped<IBussiness, BussinessLayer>();
             services.AddControllersWithViews();           
         }
         public static string parseElephantSQLURL(string uriString)
